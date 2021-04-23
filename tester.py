889e61f7012890f80790c1f5d1d25eb0b6ef6024
pde-bakk <pde-bakk@student.codam.nl>
2021-04-23 18:07:35 +0200

    my baby

--- tester.py
+++ tester.py
@@ -8,10 +8,8 @@ import sh
 DEFAULT_AMOUNT = 4
 
 
-def tester(*args) -> tuple:
-	if len(args) > 1:
-		my_nbs = list(args)
-	else:
+def create_array(args):
+	if len(args) == 1 and type(args[0]) == int:
 		amount_nbs = args[0]
 		my_nbs = set()
 		for i in range(amount_nbs):
@@ -19,23 +17,69 @@ def tester(*args) -> tuple:
 		my_nbs = list(my_nbs)
 		random.shuffle(my_nbs)
 		random.shuffle(my_nbs)
-	arg = [str(x) for x in my_nbs]
+	else:
+		my_nbs = args
+	return [str(x) for x in my_nbs]
 
-	# print(f'list_nbs is {list_nbs}, arg is {arg}')
+
+def test_checker_parsing(*args) -> str:
+	arg = create_array(list(args))
+	status, output = subprocess.getstatusoutput(f'./checker "{arg}"')
+	return output
+
+
+def test_checker_operations(nbs, checker_input) -> str:
+	checker_input = "\n".join(checker_input)
+	print(f'checker_input is {checker_input}')
+	status, output = subprocess.getstatusoutput(f'echo "{checker_input}" | ./checker {nbs}')
+	print(f'stauts is {status}, output is {output}')
+	return output
+
+
+def assert_checker_outcome(test_output, desired_outcome):
+	# if test_output != desired_outcome:
+		# print(f"Test failed for '{test_output}'")
+	assert test_output == desired_outcome
+	return 5
+
+
+def tester(*args) -> tuple:
+	arg = create_array(args)
 	p1 = subprocess.Popen(['./push_swap'] + arg, stdout=subprocess.PIPE)
-	p2 = subprocess.Popen(['./checker'] + arg, stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+	p2 = subprocess.Popen(['./checker'] + arg, stdin=p1.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	output = p2.stdout.read().decode('ascii')
+	errput = p2.stderr.read().decode('ascii')
 	split_output = output.split()
+
+	# print(f'errput is {errput}')
+	print(f'output is {output}')
+
 	return split_output[0][1:-1], int(split_output[2])
 
 
 def assert_outcome(test_input, desired_outcome):
 	assert test_input[0] == desired_outcome
+	return 5
 
 
 def moulinette(argv):
-	tester(0, 1, 2)
-	# tester(3)
+	# score = 0
+	# try:
+	# 	# score += assert_checker_outcome(test_checker_parsing('hallo', 1, 2), 'Error')
+	# 	# score += assert_checker_outcome(test_checker_parsing(2, 1, 2), 'Error')
+	# 	# score += assert_checker_outcome(test_checker_parsing(238972, 2147483649, 789), 'Error')
+	# 	# score += assert_checker_outcome(test_checker_parsing(""), 'Error')
+	# 	# score += assert_checker_outcome(test_checker_operations([0, 9, 1, 8, 2, 7, 3, 6, 4, 5], ['sa', 'pb', 'rr']), 'KO')
+	# 	# score += tester(0, 1, 2)
+	# 	print(f'All tests passed, final score is {score}')
+	# except AssertionError as e:
+	# 	print(f'Test failed because {e}, final score is {score}')
+
+	print(tester(1043, 6630, 239, 4551, 9159))
+	print(tester(1750, 9715, 3048, 3453, 9149))
+	print(tester(8, 5, 6, 3, 2, 1))
+	print(tester(5))
+	print(tester(27))
 
 
 if __name__ == "__main__":
