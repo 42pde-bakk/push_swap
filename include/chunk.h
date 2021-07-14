94677e959d9e0a5edcfcbd16a8759d806a604b5d
Peer de Bakker <pde-bakk@student.codam.nl>
2021-07-14 11:35:53 +0200

    added chunk_increase()

--- include/chunk.h
+++ include/chunk.h
@@ -18,6 +18,7 @@ typedef struct s_chunk {
 
 t_chunk 	chunk_init(int nb, int size);
 void		chunk_decrease(t_chunk *chunk);
+void		chunk_increase(t_chunk *chunk);
 
 int get_chunk_size(t_stack *stack);
 bool		is_within_chunk(int item, const t_chunk *chunk);
--- src/chunk/chunk.c
+++ src/chunk/chunk.c
@@ -16,17 +16,28 @@ t_chunk	chunk_init(const int nb, const int size)
 	return (out);
 }
 
-void	chunk_decrease(t_chunk *chunk)
+static void	update_min_max(t_chunk *chunk)
 {
-	--chunk->nb;
 	chunk->min = chunk->nb * chunk->size;
 	chunk->max = chunk->min + chunk->size - 1;
 }
 
+void	chunk_decrease(t_chunk *chunk)
+{
+	--chunk->nb;
+	update_min_max(chunk);
+}
+
+void	chunk_increase(t_chunk *chunk)
+{
+	++chunk->nb;
+	update_min_max(chunk);
+}
+
 bool	is_within_chunk(const int item, const t_chunk *chunk)
 {
-	return (item / chunk->size == chunk->nb);
-//	return (chunk->min <= item && item <= chunk->max);
+//	return (item / chunk->size == chunk->nb);
+	return (chunk->min <= item && item <= chunk->max);
 }
 
 int get_chunk_size(t_stack *stack)
