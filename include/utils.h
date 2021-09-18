c13aaef4105aaffe32865675cd79d5bd30898da3
Peer de Bakker <36886300+pde-bakk@users.noreply.github.com>
2021-09-18 14:56:46 +0200

    Revert "Restyle Norm: wack"

--- include/utils.h
+++ include/utils.h
@@ -3,19 +3,19 @@
 //
 
 #ifndef UTILS_H
-#define UTILS_H
+# define UTILS_H
 
-#include "stack.h"
-#include <stdlib.h>
+# include <stdlib.h>
+# include "stack.h"
 
-void fatal_error(t_collection *stacks, const char *s);
-int cleanup(t_collection *stacks);
+void			fatal_error(t_collection *stacks, const char *s);
+int				cleanup(t_collection *stacks);
 
-unsigned int amount_digits(int n);
-int atoi_exit_on_error(const char *s, bool *error);
-unsigned int get_max_bits(unsigned int max_num);
-void clearscreen(void);
-size_t get_max_element(const t_stack *stack);
+unsigned int	amount_digits(int n);
+int				atoi_exit_on_error(const char *s, bool *error);
+unsigned int	get_max_bits(unsigned int max_num);
+void			clearscreen(void);
+size_t			get_max_element(const t_stack *stack);
 
-// UTILS_H
+//UTILS_H
 #endif
