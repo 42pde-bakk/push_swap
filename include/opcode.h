b30fb474822a898c81292d0142352266e3261cc6
Restyled.io <commits@restyled.io>
2021-09-18 12:56:53 +0000

    Restyled by clang-format

--- include/opcode.h
+++ include/opcode.h
@@ -3,23 +3,22 @@
 //
 
 #ifndef OPCODE_H
-# define OPCODE_H
+#define OPCODE_H
 
-typedef enum e_opcode
-{
-    SA,
-    SB,
-    SS,
-    PA,
-    PB,
-    RA,
-    RB,
-    RR,
-    RRA,
-    RRB,
-    RRR,
-    ERROR
+typedef enum e_opcode {
+  SA,
+  SB,
+  SS,
+  PA,
+  PB,
+  RA,
+  RB,
+  RR,
+  RRA,
+  RRB,
+  RRR,
+  ERROR
 } t_opcode;
 
-//OPCODE_H
+// OPCODE_H
 #endif
--- include/push_swap.h
+++ include/push_swap.h
@@ -3,14 +3,14 @@
 //
 
 #ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
+#define PUSH_SWAP_H
 
-# include "utils.h"
-# include "stack.h"
+#include "stack.h"
+#include "utils.h"
 
-int		is_valid_arg(const char *s);
-void	parse_array(t_collection *stacks, char **argv, int i);
-void	parse_split_array(t_collection *coll, char *s);
+int is_valid_arg(const char *s);
+void parse_array(t_collection *stacks, char **argv, int i);
+void parse_split_array(t_collection *coll, char *s);
 
-//PUSH_SWAP_H
+// PUSH_SWAP_H
 #endif
